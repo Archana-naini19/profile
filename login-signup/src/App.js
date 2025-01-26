cd9a90215fe6ab361410ea7a9d6fdc4804ebe193
Archana.Naini <archanaarchu920@gmail.com>
2025-01-26 19:17:15 +0530

    login signup code

---
+++ login-signup/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Loginsignup from './componants/assets/loginsignup/Loginsignup.js';
+
+function App() {
+  return (
+    <div>
+      <Loginsignup></Loginsignup>
+    </div>
+  );
+}
+
+export default App;
